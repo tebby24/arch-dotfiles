81d929e41e37d0788fe40fb48d3e76734659df7e
Teddy Gonyea <enterted@gmail.com>
2025-08-31 21:41:30 -0400

    nvim update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,17 @@
-vim.cmd.colorscheme("lunaperche")
+vim.g.mapleader = " "
 
+vim.opt.number = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.swapfile = false
+
+
+vim.pack.add({
+  "https://github.com/vague2k/vague.nvim",
+})
+
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
+
