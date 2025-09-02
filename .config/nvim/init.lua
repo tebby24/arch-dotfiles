655e9e1e7351ee7c18ee91fe0eb333dea521a6b5
Teddy Gonyea <enterted@gmail.com>
2025-09-02 13:36:44 -0400

    nvim stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,17 +9,33 @@ vim.opt.winborder = "rounded"
 
 vim.opt.swapfile = false
 
-
 vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/vague2k/vague.nvim" },
+    { src = "https://github.com/stevearc/oil.nvim" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
+    { src = "https://github.com/echasnovski/mini.pick" },
     { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = "https://github.com/iamcco/markdown-preview.nvim",    build = "cd app && yarn install" },
+    { src = "https://github.com/lervag/vimtex" },
 })
 
 require "mini.pick".setup()
 require "oil".setup()
+
+vim.g.mkdp_browser = "chromium"
+
+-- Vimtex configuration
+vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_compiler_method = 'latexmk'
+vim.g.vimtex_compiler_latexmk = {
+    options = {
+        '-pdf',
+        '-shell-escape',
+        '-verbose',
+        '-file-line-error',
+        '-interaction=nonstopmode',
+    },
+}
 
 local map = vim.keymap.set
 map('n', '<leader>f', ":Pick files<CR>")
@@ -27,18 +43,16 @@ map('n', '<leader>h', ":Pick help<CR>")
 map('n', '<leader>e', ":Oil<CR>")
 map('n', '<leader>lf', vim.lsp.buf.format)
 
-vim.lsp.enable(
-    { "lua_ls",
-    "tinymist",
-    "clangd",
-    "black",
-    "ruff",
-}
-)
+local lspconfig = require('lspconfig')
+
+lspconfig.lua_ls.setup({})
+lspconfig.tinymist.setup({})
+lspconfig.clangd.setup({})
+lspconfig.texlab.setup({}) 
+
 
 -- colorscheme
 require "vague".setup({ transparent = true })
 vim.cmd("colorscheme vague")
 vim.cmd("hi Normal guibg=NONE")
 vim.cmd(":hi statusline guibg=NONE")
-
